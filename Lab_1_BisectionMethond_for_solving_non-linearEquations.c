ca3d957da5366f66afd3afcda269987a48a7eeb9
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-17 23:12:52 +0545

    Update Lab_1_BisectionMethond_for_solving_non-linearEquations.c

--- Lab_1_BisectionMethond_for_solving_non-linearEquations.c
+++ Lab_1_BisectionMethond_for_solving_non-linearEquations.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 double f(double x) {
-    return (x * sin(x) + cos(x));  
+    return (x * sin(x) + cos(x));
 }
 
-void main() {
+int main() {
     double a, b, E, c;
+    int maxIter = 1000, iter = 0;
 
     printf("Enter initial interval (a, b): ");
     scanf("%lf %lf", &a, &b);
 
     printf("Enter error tolerance (E): ");
     scanf("%lf", &E);
-
     if (f(a) * f(b) > 0) {
         printf("Invalid interval. Root may not exist between (a, b).\n");
-        return;  
+        return 0;
     }
-
     do {
         c = (a + b) / 2.0;
 
@@ -27,8 +25,14 @@ void main() {
         else
             a = c;
 
+        iter++;
+        if (iter > maxIter) {
+            printf("Method did not converge.\n");
+            return 0;
+        }
+
     } while (fabs(f(c)) > E);
 
-    printf("Root = %.6f\n", c); 
-    getch();
+    printf("Root = %.6f (after %d iterations)\n", c, iter);
+    return 0;
 }
